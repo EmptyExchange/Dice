afe69ca3d069e6fa34b66f70a6c5bf8a0aa00245
EmptyExchange <Michael.young07@outlook.com>
2024-05-09 09:32:15 -0500

    added note to function

--- diceIndex.js
+++ diceIndex.js
@@ -1,6 +1,6 @@
 const diceResult = [];
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
+//function rolls dice to get 6 random numbers which will be assigned to dice faces
 function rollDice() {
     for (i = 6; i > 0; i--){
         let diceNumber = getRandomNumber(1, 6);
