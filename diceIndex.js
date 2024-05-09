4514d1670d5497c2209dcc10851b8a4c9b528761
EmptyExchange <Michael.young07@outlook.com>
2024-05-09 10:31:55 -0500

    assigned variable to each die

--- diceIndex.js
+++ diceIndex.js
@@ -1,4 +1,5 @@
 const diceResult = [];
+
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 //function rolls dice to get 6 random numbers which will be assigned to dice faces
 function rollDice() {
@@ -6,7 +7,19 @@ function rollDice() {
         let diceNumber = getRandomNumber(1, 6);
         diceResult.push(diceNumber);
     }
-    console.log(diceResult)
+   console.log(diceResult)
+const firstDie = diceResult[0];
+const secondDie = diceResult[1];
+const thirdDie = diceResult[2];
+const fourthDie = diceResult[3];
+const fifthDie = diceResult[4];
+const sixthDie = diceResult[5];
+console.log(firstDie);
+console.log(secondDie);
+console.log(thirdDie);
+console.log(fourthDie);
+console.log(fifthDie);
+console.log(sixthDie);
 }
 
 rollDice();
